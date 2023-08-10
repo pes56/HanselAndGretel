a22567cdfc23d6950a292fd779717e9b5e9a1ca6
C0smicBUndle <141589960+C0smicBUndle@users.noreply.github.com>
2023-08-10 02:41:23 -0400

    Mini Updates
    added keybox to open

---
+++ HanselAndGretel/Assets/Scripts/S_GamePlay/KeyBox.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KeyBox : MonoBehaviour
+{
+    public void InsideKeyBox()
+    {
+        SceneManager.LoadScene("KeyBox");
+    }
+}
