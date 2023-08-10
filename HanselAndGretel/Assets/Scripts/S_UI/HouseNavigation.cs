e1ab12e49b843110b692e12096f5fa69e30ee77f
C0smicBUndle <141589960+C0smicBUndle@users.noreply.github.com>
2023-08-10 02:11:57 -0400

    So much updates
    added house layout, tutorial screen

---
+++ HanselAndGretel/Assets/Scripts/S_UI/HouseNavigation.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HouseNavigation : MonoBehaviour
+{
+    public void MainRoom()
+    {
+        SceneManager.LoadScene("HallWay");
+    }
+
+    public void HallwayF()
+    {
+        SceneManager.LoadScene("Basement");
+    }
+
+    public void HallwayR()
+    {
+        SceneManager.LoadScene("HanselRoom");
+    }
+
+    public void HallwayL()
+    {
+        SceneManager.LoadScene("Kitchen");
+    }
+}
